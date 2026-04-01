d29b007a38bd09696161d30442b0e54a49b34db9
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-01 17:22:06 -0400

    Add SpinnerController for updating UI spinner

---
+++ Logic/SpinnerController.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+
+namespace POPSManager.Logic
+{
+    public class SpinnerController
+    {
+        private readonly Action<string> update;
+        private bool running;
+
+        public SpinnerController(Action<string> update)
+        {
+            this.update = update;
+        }
+
+        public void Start()
+        {
+            running = true;
+            Task.Run(async () =>
+            {
+                string frames = "|/-\\";
+                int i = 0;
+
+                while (running)
+                {
+                    update(frames[i % frames.Length]);
+                    i++;
+                    await Task.Delay(100);
+                }
+
+                update("");
+            });
+        }
+
+        public void Stop()
+        {
+            running = false;
+        }
+    }
+}
