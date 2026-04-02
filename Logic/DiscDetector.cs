12c1a3aa06b5662fc2fc9f00a9a543cd03b9dc4d
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-02 09:29:57 -0400

    Update DiscDetector.cs

--- Logic/DiscDetector.cs
+++ Logic/DiscDetector.cs
@@ -4,7 +4,7 @@ namespace POPSManager.Logic
 {
     public static class DiscDetector
     {
-        public static string Detect(string name)
+        public static string? Detect(string name)
         {
             var match = Regex.Match(name, @"(Disc|Disk|CD)\s*([1-9])", RegexOptions.IgnoreCase);
             if (match.Success)
