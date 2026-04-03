ba5e130873ed0eb7bdbf8aaebaa23a83f84cb0a2
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-03 01:27:55 -0400

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,18 @@
 using System.Windows;
-using Application = System.Windows.Application;
+using POPSManager.Services;
 
 namespace POPSManager
 {
     public partial class App : Application
     {
+        public AppServices Services { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Inicializar todos los servicios de la aplicación
+            Services = new AppServices();
+        }
     }
 }
