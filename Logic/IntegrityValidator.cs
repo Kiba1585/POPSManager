b1858afc6b9d8a6cd4eb7f0fd2aa27a30a8eae22
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-07 15:03:24 -0400

    Update IntegrityValidator.cs

--- Logic/IntegrityValidator.cs
+++ Logic/IntegrityValidator.cs
@@ -1,49 +1,17 @@
-using System;
 using System.IO;
-using System.Text;
 
 namespace POPSManager.Logic
 {
     public static class IntegrityValidator
     {
-        private const int MinSizeBytes = 100_000;
-        private const int HeaderSize = 0x800;
-        private static readonly byte[] ExpectedHeader = Encoding.ASCII.GetBytes("PSX");
-
-        public static bool Validate(string path)
+        public static bool Validate(string vcdPath)
         {
-            if (!File.Exists(path))
+            var info = new FileInfo(vcdPath);
+
+            if (info.Length < 1_000_000)
                 return false;
 
-            var info = new FileInfo(path);
-
-            if (info.Length < MinSizeBytes)
-                return false;
-
-            if (!path.EndsWith(".vcd", StringComparison.OrdinalIgnoreCase))
-                return false;
-
-            try
-            {
-                using var fs = File.OpenRead(path);
-
-                byte[] header = new byte[3];
-                fs.Read(header, 0, 3);
-
-                if (!header.AsSpan().SequenceEqual(ExpectedHeader))
-                    return false;
-
-                long dataSize = info.Length - HeaderSize;
-
-                if (dataSize <= 0 || dataSize % 2048 != 0)
-                    return false;
-
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
+            return true;
         }
     }
 }
