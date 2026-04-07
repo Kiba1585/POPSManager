273ea5dd0942ff75fbc35df88650f58b8473226f
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-07 16:37:16 -0400

    Create ConvertedGame.cs

---
+++ Models/ConvertedGame.cs
@@ -0,0 +1,10 @@
+namespace POPSManager.Models
+{
+    public class ConvertedGame
+    {
+        public required string VcdPath { get; set; }
+        public required string BaseName { get; set; }
+        public int DiscNumber { get; set; }
+        public bool IsMultiDisc { get; set; }
+    }
+}
