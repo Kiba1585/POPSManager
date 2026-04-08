27e62e6a65f46d73407a1843cfae3dfdeabf91e5
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-08 03:04:05 -0400

    Create GameInfo.cs

---
+++ Models/GameInfo.cs
@@ -0,0 +1,9 @@
+namespace POPSManager.Models
+{
+    public class GameInfo
+    {
+        public string Name { get; set; } = "";
+        public int DiscNumber { get; set; } = 1;
+        public string? CoverUrl { get; set; }
+    }
+}
