70a53b67135c43b435e5bd425caed9b687ee9c3e
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-09 05:38:43 -0400

    Update UiNotification.cs

--- Models/UiNotification.cs
+++ Models/UiNotification.cs
@@ -1,13 +1,7 @@
+using POPSManager.Models;
+
 namespace POPSManager.Models
 {
-    public enum NotificationType
-    {
-        Success,
-        Error,
-        Warning,
-        Info
-    }
-
     public class UiNotification
     {
         public NotificationType Type { get; set; }
