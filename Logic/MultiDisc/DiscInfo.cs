66725691010231905f93a8ca36ea680b0ce96fd1
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-09 03:47:01 -0400

    Create DiscInfo.cs

---
+++ Logic/MultiDisc/DiscInfo.cs
@@ -0,0 +1,11 @@
+namespace POPSManager.Logic
+{
+    public class DiscInfo
+    {
+        public string Path { get; set; } = "";
+        public int DiscNumber { get; set; }
+        public string GameId { get; set; } = "";
+        public string FolderName { get; set; } = "";
+        public string FileName { get; set; } = "";
+    }
+}
