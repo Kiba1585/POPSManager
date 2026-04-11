a2c3ade699d8446f798a8b42f8b53e01224fd77e
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-11 11:17:03 -0400

    Create PvdInfo.cs

---
+++ Core/Integrity/PvdInfo.cs
@@ -0,0 +1,9 @@
+namespace POPSManager.Core.Integrity
+{
+    public class PvdInfo
+    {
+        public string Identifier { get; set; } = "";
+        public string VolumeName { get; set; } = "";
+        public string SystemId { get; set; } = "";
+    }
+}
