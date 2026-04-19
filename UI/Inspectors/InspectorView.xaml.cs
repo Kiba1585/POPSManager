438a79069861fcedc4888ccc08e8255a20454212
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-19 19:34:38 -0400

    Actualizar InspectorView.xaml.cs

--- UI/Inspectors/InspectorView.xaml.cs
+++ UI/Inspectors/InspectorView.xaml.cs
@@ -2,7 +2,7 @@ using System.Windows.Controls;
 
 namespace POPSManager.UI.Inspectors
 {
-    public partial class InspectorView : UserControl
+    public partial class InspectorView : System.Windows.Controls.UserControl
     {
         public InspectorView()
         {
