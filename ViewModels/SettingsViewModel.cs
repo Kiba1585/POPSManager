9fe72f2b2a15d0e5b8ec8588759e534edd86476c
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-20 00:59:21 -0400

    Actualizar SettingsViewModel.cs

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -26,8 +26,8 @@ namespace POPSManager.ViewModels
         private string _rootPath = string.Empty;
         private string _popsPath = string.Empty;
         private string _appsPath = string.Empty;
-        private string _lngPath = string.Empty;   // NUEVO
-        private string _thmPath = string.Empty;   // NUEVO
+        private string _lngPath = string.Empty;
+        private string _thmPath = string.Empty;
         private string _elfPath = string.Empty;
         private bool _darkMode;
         private bool _notificationsEnabled;
@@ -35,6 +35,8 @@ namespace POPSManager.ViewModels
         private AutoBehavior _normalizeNamesBehavior;
         private AutoBehavior _groupMultiDiscBehavior;
         private AutoBehavior _downloadCoversBehavior;
+        private AutoBehavior _lngBehavior;
+        private AutoBehavior _thmBehavior;
         private AppLanguage _selectedLanguage;
 
         public SettingsViewModel()
@@ -58,8 +60,8 @@ namespace POPSManager.ViewModels
             ChangeRootFolderCommand = new RelayCommand(async () => await ChangeRootFolderAsync());
             ChangePopsPathCommand = new RelayCommand(async () => await ChangePopsPathAsync());
             ChangeAppsPathCommand = new RelayCommand(async () => await ChangeAppsPathAsync());
-            ChangeLngPathCommand = new RelayCommand(async () => await ChangeLngPathAsync());     // NUEVO
-            ChangeThmPathCommand = new RelayCommand(async () => await ChangeThmPathAsync());     // NUEVO
+            ChangeLngPathCommand = new RelayCommand(async () => await ChangeLngPathAsync());
+            ChangeThmPathCommand = new RelayCommand(async () => await ChangeThmPathAsync());
             SelectElfCommand = new RelayCommand(async () => await SelectElfAsync());
             OpenProgramFolderCommand = new RelayCommand(OpenProgramFolder);
             SaveSettingsCommand = new RelayCommand(async () => await SaveSettingsAsync());
@@ -85,8 +87,8 @@ namespace POPSManager.ViewModels
         public string RootPath { get => _rootPath; set => SetProperty(ref _rootPath, value); }
         public string PopsPath { get => _popsPath; set => SetProperty(ref _popsPath, value); }
         public string AppsPath { get => _appsPath; set => SetProperty(ref _appsPath, value); }
-        public string LngPath { get => _lngPath; set => SetProperty(ref _lngPath, value); }     // NUEVO
-        public string ThmPath { get => _thmPath; set => SetProperty(ref _thmPath, value); }     // NUEVO
+        public string LngPath { get => _lngPath; set => SetProperty(ref _lngPath, value); }
+        public string ThmPath { get => _thmPath; set => SetProperty(ref _thmPath, value); }
         public string ElfPath { get => _elfPath; set => SetProperty(ref _elfPath, value); }
 
         public bool DarkMode
@@ -125,11 +127,23 @@ namespace POPSManager.ViewModels
             set { if (SetProperty(ref _downloadCoversBehavior, value)) _ = SaveSettingsAsync(); }
         }
 
+        public AutoBehavior LngBehavior
+        {
+            get => _lngBehavior;
+            set { if (SetProperty(ref _lngBehavior, value)) _ = SaveSettingsAsync(); }
+        }
+
+        public AutoBehavior ThmBehavior
+        {
+            get => _thmBehavior;
+            set { if (SetProperty(ref _thmBehavior, value)) _ = SaveSettingsAsync(); }
+        }
+
         public ICommand ChangeRootFolderCommand { get; }
         public ICommand ChangePopsPathCommand { get; }
         public ICommand ChangeAppsPathCommand { get; }
-        public ICommand ChangeLngPathCommand { get; }      // NUEVO
-        public ICommand ChangeThmPathCommand { get; }      // NUEVO
+        public ICommand ChangeLngPathCommand { get; }
+        public ICommand ChangeThmPathCommand { get; }
         public ICommand SelectElfCommand { get; }
         public ICommand OpenProgramFolderCommand { get; }
         public ICommand SaveSettingsCommand { get; }
@@ -139,8 +153,8 @@ namespace POPSManager.ViewModels
             RootPath = _paths.RootFolder;
             PopsPath = _paths.PopsFolder;
             AppsPath = _paths.AppsFolder;
-            LngPath = _paths.LngFolder;      // NUEVO
-            ThmPath = _paths.ThmFolder;      // NUEVO
+            LngPath = _paths.LngFolder;
+            ThmPath = _paths.ThmFolder;
             ElfPath = _paths.PopstarterElfPath;
             DarkMode = _settings.DarkMode;
             NotificationsEnabled = _settings.NotificationsEnabled;
@@ -148,6 +162,8 @@ namespace POPSManager.ViewModels
             NormalizeNamesBehavior = _settings.Automation.Conversion;
             GroupMultiDiscBehavior = _settings.Automation.MultiDisc;
             DownloadCoversBehavior = _settings.Automation.Covers;
+            LngBehavior = _settings.Automation.Lng;
+            ThmBehavior = _settings.Automation.Thm;
             SelectedLanguage = _settings.Language;
         }
 
@@ -234,7 +250,7 @@ namespace POPSManager.ViewModels
             }
         }
 
-        private async Task ChangeLngPathAsync()     // NUEVO
+        private async Task ChangeLngPathAsync()
         {
             var dialog = new OpenFolderDialog { Title = "Seleccionar carpeta de idiomas (LNG)" };
             if (dialog.ShowDialog() != true) return;
@@ -259,7 +275,7 @@ namespace POPSManager.ViewModels
             }
         }
 
-        private async Task ChangeThmPathAsync()     // NUEVO
+        private async Task ChangeThmPathAsync()
         {
             var dialog = new OpenFolderDialog { Title = "Seleccionar carpeta de temas (THM)" };
             if (dialog.ShowDialog() != true) return;
@@ -338,6 +354,8 @@ namespace POPSManager.ViewModels
                 _settings.Automation.Conversion = NormalizeNamesBehavior;
                 _settings.Automation.MultiDisc = GroupMultiDiscBehavior;
                 _settings.Automation.Covers = DownloadCoversBehavior;
+                _settings.Automation.Lng = LngBehavior;
+                _settings.Automation.Thm = ThmBehavior;
                 _settings.Language = SelectedLanguage;
 
                 await _settings.SaveAsync();
